0adb6685f480d63409e4f53ddd04e53cfa5a977e
shaksworld <shakaaliakwe@gmail.com>
2023-07-05 10:08:57 +0100

    first commit

---
+++ src/main/java/com/shaks/fieldmanagementsystem/FieldManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shaks.fieldmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shaks/fieldmanagementsystem/FieldManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaks.fieldmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
